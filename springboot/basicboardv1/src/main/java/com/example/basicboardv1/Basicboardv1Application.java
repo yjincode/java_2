b4d7bff770ae2dc8d7650e86f697257b16afa3e4
yjin <yjincode@naver.com>
2025-02-25 11:25:19 +0900

    new project

---
+++ springboot/basicboardv1/src/main/java/com/example/basicboardv1/Basicboardv1Application.java
@@ -0,0 +1,13 @@
+package com.example.basicboardv1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Basicboardv1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Basicboardv1Application.class, args);
+    }
+
+}
---
+++ springboot/basicboardv1/src/test/java/com/example/basicboardv1/Basicboardv1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.basicboardv1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Basicboardv1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
